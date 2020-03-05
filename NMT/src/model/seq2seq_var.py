92f0562ced3c5487c34892a931fa92e71513d904
Leo Dong <leodong0315@gmail.com>
2020-03-04 23:36:30 -0800

    minor

--- NMT/src/model/seq2seq_var.py
+++ NMT/src/model/seq2seq_var.py
@@ -481,8 +481,9 @@ class LatentJoint(nn.Module):
         super(LatentJoint, self).__init__()
         self.latent_dim = params.latent_dim
         self.hidden_dim = params.hidden_dim
-        self.mu = nn.Linear(self.hidden_dim * 2, latent_dim)
-        self.var = nn.Linear(self.hidden_dim * 2, latent_dim)
+        self.latent_dim = params.latent_dim
+        self.mu = nn.Linear(self.hidden_dim * 2, self.latent_dim)
+        self.var = nn.Linear(self.hidden_dim * 2, self.latent_dim)
 
     def reparameterize(self, mu, logvar):
         """ Resample from the latent space distribution, specified by mu and logvar.
