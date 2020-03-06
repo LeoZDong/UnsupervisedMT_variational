acbb596d711b01b98aedfec64775c0a3a62d00d1
Leo Dong <leodong0315@gmail.com>
2020-03-05 20:17:47 -0800

    change

--- NMT/get_data_enfr.sh
+++ NMT/get_data_enfr.sh
@@ -167,6 +167,7 @@ echo "FR monolingual data concatenated in: $TGT_RAW"
 
 # check number of lines
 echo "$(wc -l < $SRC_RAW)"
+echo "$(wc -l < $TGT_RAW)"
 if ! [[ "$(wc -l < $SRC_RAW)" -eq "$N_MONO" ]]; then echo "ERROR: Number of lines doesn't match! Be sure you have $N_MONO sentences in your EN monolingual data."; exit; fi
 if ! [[ "$(wc -l < $TGT_RAW)" -eq "$N_MONO" ]]; then echo "ERROR: Number of lines doesn't match! Be sure you have $N_MONO sentences in your FR monolingual data."; exit; fi
 
