8c585377842e63de6a60635b800dad16ba4e8763
Leo Dong <leodong0315@gmail.com>
2020-03-11 14:07:28 -0700

    debug

--- NMT/src/model/__init__.py
+++ NMT/src/model/__init__.py
@@ -12,8 +12,8 @@ from collections import namedtuple
 LSTM_PARAMS = ['weight_ih_l%i', 'weight_hh_l%i', 'bias_ih_l%i', 'bias_hh_l%i']
 BILSTM_PARAMS = LSTM_PARAMS + ['%s_reverse' % x for x in LSTM_PARAMS]
 
-# LatentState = namedtuple('LatentState', 'dec_input, dis_input, input_len, enc_hiddens')
-LatentState = namedtuple('LatentState', 'dec_input, dis_input, input_len')
+LatentState = namedtuple('LatentState', 'dec_input, dis_input, input_len, enc_hiddens')
+# LatentState = namedtuple('LatentState', 'dec_input, dis_input, input_len')
 
 def check_mt_model_params(params):
     """
