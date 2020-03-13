54dd851c17b710bac70cfff10eb4ebdf10581988
Leo Dong <leodong0315@gmail.com>
2020-03-13 03:09:21 -0700

    update

--- NMT/main_var.py
+++ NMT/main_var.py
@@ -301,19 +301,19 @@ def main(params):
             if params.lambda_xe_para > 0:
                 for lang1, lang2 in params.para_directions:
                     trainer.enc_dec_step(lang1, lang2, params.lambda_xe_para)
-            torch.cuda.empty_cache()
+                    torch.cuda.empty_cache()
 
             # MT training (back-parallel data)
             if params.lambda_xe_back > 0:
                 for lang1, lang2 in params.back_directions:
                     trainer.enc_dec_step(lang1, lang2, params.lambda_xe_back, back=True)
-            torch.cuda.empty_cache()
+                    torch.cuda.empty_cache()
 
             # autoencoder training (monolingual data)
             if params.lambda_xe_mono > 0:
                 for lang in params.mono_directions:
                     trainer.enc_dec_step(lang, lang, params.lambda_xe_mono)
-            torch.cuda.empty_cache()
+                    torch.cuda.empty_cache()
 
             # AE - MT training (on the fly back-translation)
             if params.lambda_xe_otfd > 0 or params.lambda_xe_otfa > 0:
